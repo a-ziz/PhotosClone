1c5bc7d7702ef9d00a240a796d2fa080661e0836
Aziz <azizahmed.dev@gmail.com>
2023-09-03 23:39:06 -0400

    POST method with validation

--- src/main/java/com/test/aziz/photos/clone/Photo.java
+++ src/main/java/com/test/aziz/photos/clone/Photo.java
@@ -1,8 +1,11 @@
 package com.test.aziz.photos.clone;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class Photo {
 
   private String id;
+  @NotEmpty
   private String fileName;
   // to add - raw data later
 
--- src/main/java/com/test/aziz/photos/clone/PhotosController.java
+++ src/main/java/com/test/aziz/photos/clone/PhotosController.java
@@ -1,16 +1,11 @@
 package com.test.aziz.photos.clone;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class PhotosController {
@@ -49,5 +44,12 @@ public class PhotosController {
   public void delete(@PathVariable String id){
     Photo photo = db.remove(id);
     if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+  }
+
+  @PostMapping("/photos")
+  public Photo create (@RequestBody @Valid Photo photo){
+    photo.setId(UUID.randomUUID().toString());
+    db.put(photo.getId(), photo);
+    return photo;
   }
 }
